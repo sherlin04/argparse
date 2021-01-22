2f01761def34d4a62bd76a97d16b7ccf9056134d
Dave Enrico <denricoNBHS@users.noreply.github.com>
2021-01-22 15:10:40 -0500

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,11 @@
+import argparse
+from statistics import mean, median
+
+""" 
+Write a command line tool that accepts a list of (one or more) 
+numbers and returns their mean by default or, if the '-m' or '--median'
+options are included, their median instead.
+
+The mean and median functions from the statistics module have been
+included for you.
+"""
